05a75147ff79edd269b3577bc08e0c77c469984a
hy.moon <moraramee@gmail.com>
2022-06-22 14:57:54 +0900

    Add security config

---
+++ auth-service/src/main/java/com/hometail/authservice/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.hometail.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests().anyRequest().permitAll()
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable();
+
+        return http.build();
+    }
+}
