14174728d6a3e6740a202cd7bd9f6758325b12a9
hy.moon <moraramee@gmail.com>
2022-06-22 15:02:05 +0900

    Update auth service
    addAccount()

--- auth-service/src/main/java/com/hometail/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/hometail/authservice/service/AuthService.java
@@ -1,8 +1,22 @@
 package com.hometail.authservice.service;
 
+import com.hometail.authservice.domain.Account;
+import com.hometail.authservice.domain.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class AuthService {
 
+    private final AccountRepository accountRepository;
+
+    public Account addAccount(Account account) {
+
+        return accountRepository.save(account);
+    }
+
+    public void getJwt() {
+
+    }
 }
