35e3c7eb5b64494b166a7036968aea21538cac23
hy.moon <moraramee@gmail.com>
2022-07-06 18:12:01 +0900

    feat: Add bindexception handler

--- auth-service/src/main/java/com/hometail/authservice/exception/RestExceptionHandler.java
+++ auth-service/src/main/java/com/hometail/authservice/exception/RestExceptionHandler.java
@@ -3,11 +3,22 @@ package com.hometail.authservice.exception;
 import com.hometail.authservice.dto.response.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<?> bindError(BindException exception) {
+
+        return ResponseEntity.badRequest()
+                .body(ErrorResponseDto.builder()
+                        .httpStatus(HttpStatus.BAD_REQUEST)
+                        .exception(exception).build());
+    }
 
     @ExceptionHandler(InvalidRequestException.class)
     public ResponseEntity<?> accountServiceError(InvalidRequestException exception) {
